019d9e93f61d64d950f3fdbade24ba85790c3bd8
Pierre <jpcampoverde9801@hotmail.com>
2023-05-14 21:58:07 -0300

    Set up favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import Footer from './components/footer';
 import Navbar from './components/navbar';
 import './globals.css';
 export const metadata = {
+  icons: {
+    icon: '/favicon.ico',
+  },
   title: 'Playrr',
   description:
     'Playrr: Tu nueva app en Buenos Aires para conectar con deportistas locales y encontrar partidos de fútbol, baloncesto y voleibol en tu área. Regístrate, únete a partidos, juega y conecta con la comunidad. Con Playrr, nunca te quedarás sin jugar. ¡Descubre tu próxima gran aventura deportiva con nosotros!',
